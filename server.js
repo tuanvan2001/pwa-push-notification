0c4a6658339ee1af24e3159a64ce80162b587ed0
Your Name <you@example.com>
2023-04-07 19:13:46 +0700

    update

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'YOUR_DATABASE_URL'
+});
+
+const messaging = admin.messaging();
+
+const message = {
+    data: {
+        title: 'New Message',
+        body: 'You have a new message'
+    },
+    token: 'FCM_TOKEN'
+};
+
+messaging.sendToDevice(message.token, message)
+    .then((response) => {
+        console.log('Notification sent successfully:', response);
+    })
+    .catch((error) => {
+        console.log('Unable to send notification:', error);
+    });
+
 
