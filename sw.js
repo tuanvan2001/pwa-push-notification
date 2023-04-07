68055c1697d73f3bb943dcb6149418818a7baf75
Your Name <you@example.com>
2023-04-07 19:19:23 +0700

    update

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('my-cache').then(function(cache) {
             return cache.addAll([
-                '/',
                 '/index.html',
                 '/manifest.json'
             ]);
